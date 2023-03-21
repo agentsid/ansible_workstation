b6a6aa0bf8d622c36e8139d350a2b24f65cad5b7
agentsid <siddharthsingh2910@gmail.com>
2023-03-21 23:48:40 +0530

    ansible

--- _start_docker.sh
+++ _start_docker.sh
@@ -2,7 +2,7 @@
 
 echo " starting docker container"
 
-docker start ansible d_host7  d_host6 d_host5 d_host3 d_host4 d_host2 d_host1
+docker restart ansible d_host7  d_host6 d_host5 d_host3 d_host4 d_host2 d_host1
 
 for i in ansible d_host7  d_host6 d_host5 d_host3 d_host4 d_host2 d_host1; do
     echo "$i"
