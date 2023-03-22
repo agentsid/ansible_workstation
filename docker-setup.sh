35c13ae05203cc10cd2d61194f9145348025d056
agentsid <siddharthsingh2910@gmail.com>
2023-03-22 23:02:44 +0530

    ansible

--- docker-setup.sh
+++ docker-setup.sh
@@ -3,7 +3,7 @@
 
 for i in d_host1 d_host2 d_host3 ; do
     echo "$i"
-    docker run -d --name='$1' siddharthadmin/ansible-worker-node /bin/bash
+    docker run -it :wq-d --name='$1' siddharthadmin/ansible-worker-node /bin/bash
 done
 
 # start/stop  docker container d_host1 d_host2 d_host3
