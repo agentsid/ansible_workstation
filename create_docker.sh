9eb39601848e1473caab926fcb6ce0c04d2b6b32
agentsid <siddharthsingh2910@gmail.com>
2023-03-22 22:38:08 +0530

    ansible

---
+++ create_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# creating docker container d_host1 d_host2 d_host3
+
+for i in d_host1 d_host2 d_host3 ; do
+    echo "$i"
+    docker run -it --name='$1' siddharthadmin/ansible-worker-node /bin/bash
+done
+
+# start/stop  docker container d_host1 d_host2 d_host3
+state=$1
+echo " $state docker container"
+for i in d_host1 d_host2 d_host3 ; do
+    echo "$i"
+    docker $state $i
+done
+
+for i in d_host1 d_host2 d_host3  ; do
+    echo "$i"
+    docker exec -it $i  service ssh restart
+done
+
