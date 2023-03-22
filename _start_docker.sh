869306a246963b13266dc7fca89d708158fe693f
agentsid <siddharthsingh2910@gmail.com>
2023-03-22 21:19:51 +0530

    ansible

--- _start_docker.sh
+++ _start_docker.sh
@@ -3,12 +3,12 @@
 echo " starting docker container"
 state=$1
 
-for i in ansible d_host5 d_host3 d_host4 d_host2 ; do
+for i in ansible d_host5 d_host3 d_host4  ; do
     echo "$i"
     docker restart $i
 done
 
-for i in ansible d_host5 d_host3 d_host4 d_host2 ; do
+for i in ansible d_host5 d_host3 d_host4  ; do
     echo "$i"
     docker exec -it $i sudo service ssh restart
 done
