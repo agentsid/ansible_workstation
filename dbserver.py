8023b73b75c940a09d0092781a009ad33efd6574
agentsid <siddharthsingh2910@gmail.com>
2023-04-03 02:24:17 +0530

    ansible

--- dbserver.py
+++ dbserver.py
@@ -1,4 +1,4 @@
-# import required libraries
+# import required
 import mysql.connector
 
 # Connect to the MySQL container
@@ -8,29 +8,15 @@ cnx = mysql.connector.connect(user='root', password='ready2go',
 # Create a cursor to execute SQL statements
 cursor = cnx.cursor()
 
-# Define the CREATE TABLE statement
-create_table = """
-CREATE TABLE test (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    age INT(11),
-    PRIMARY KEY (id)
-)
-"""
-
 # Execute the SHOW DATABASES statement
-#cursor.execute("SHOW DATABASES")
-cursor.execute(create_table)
-
-# Commit the changes
-cnx.commit()
+cursor.execute("SHOW DATABASES")
 
 # Fetch all the results
-# results = cursor.fetchall()
+results = cursor.fetchall()
 
 # Print the database names
-# for db in results:
-#     print(db[0])
+for db in results:
+    print(db[0])
 
 # Close the cursor and connection
 cursor.close()
